e98e21b9008556213c540b80d3ee047cb6342f04
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2019-02-08 00:44:37 -0800

    Double-tap functionality implemented

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,31 +1,14 @@
-import React, { Component } from 'react'
-import { 
-    Image, 
-    View,
-    Dimensions,
-    StyleSheet
-} from 'react-native'
-
-let Window = Dimensions.get('window');
-
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import PlayArea from '../components/PlayArea';
 export default class TestScreen extends Component {
   render() {
     return (
-      <View style={styles.containerStyle}>
-        <Image style={styles.mapStyle} source={require('../graphics/temp/fullsize4x6grid1424pxBorder.png')} />
+      <View>
+        <PlayArea />
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-    containerStyle: {
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    mapStyle: {
-        height: Window.height,
-        width: Window.width,
-    }
-})
 
