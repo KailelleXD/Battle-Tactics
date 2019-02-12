b974543f260cdde4995237241568ca3d8ff6a723
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2019-02-11 18:47:27 -0800

    mostly functioning minus scaling of components

---
+++ components/Test.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, StyleSheet, PanResponder, ViewPropTypes } from 'react-native';
+
+// Fallback when RN version is < 0.44
+const viewPropTypes = ViewPropTypes || View.propTypes;
+
+export default class PinchZoomView extends Component {
+  static propTypes = {
+    ...viewPropTypes,
+    scalable: PropTypes.bool,
+    minScale: PropTypes.number,
+    maxScale: PropTypes.number
+  };
+
+  static defaultProps = {
+    scalable: true,
+    minScale: 0.5,
+    maxScale: 2
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      scale: 1,
+      lastScale: 1,
+      offsetX: 0,
+      offsetY: 0,
+      lastX: 0,
+      lastY: 0,
+      lastMovePinch: false
+    };
+    this.distant = 150;
+  }
+
+  componentWillMount() {
+    this.gestureHandlers = PanResponder.create({
+      onStartShouldSetPanResponder: this._handleStartShouldSetPanResponder,
+      onMoveShouldSetPanResponder: this._handleMoveShouldSetPanResponder,
+      onPanResponderGrant: this._handlePanResponderGrant,
+      onPanResponderMove: this._handlePanResponderMove,
+      onPanResponderRelease: this._handlePanResponderEnd,
+      onPanResponderTerminationRequest: evt => true,
+      onShouldBlockNativeResponder: evt => false
+    });
+  }
+
+  _handleStartShouldSetPanResponder = (e, gestureState) => {
+    // don't respond to single touch to avoid shielding click on child components
+    return false;
+  };
+
+  _handleMoveShouldSetPanResponder = (e, gestureState) => {
+    return (
+      this.props.scalable &&
+      (Math.abs(gestureState.dx) > 2 ||
+        Math.abs(gestureState.dy) > 2 ||
+        gestureState.numberActiveTouches === 2)
+    );
+  };
+
+  _handlePanResponderGrant = (e, gestureState) => {
+    if (gestureState.numberActiveTouches === 2) {
+      let dx = Math.abs(
+        e.nativeEvent.touches[0].pageX - e.nativeEvent.touches[1].pageX
+      );
+      let dy = Math.abs(
+        e.nativeEvent.touches[0].pageY - e.nativeEvent.touches[1].pageY
+      );
+      let distant = Math.sqrt(dx * dx + dy * dy);
+      this.distant = distant;
+    }
+  };
+
+  _handlePanResponderEnd = (e, gestureState) => {
+    this.setState({
+      lastX: this.state.offsetX,
+      lastY: this.state.offsetY,
+      lastScale: this.state.scale
+    });
+  };
+
+  _handlePanResponderMove = (e, gestureState) => {
+    // zoom
+    if (gestureState.numberActiveTouches === 2) {
+      let dx = Math.abs(
+        e.nativeEvent.touches[0].pageX - e.nativeEvent.touches[1].pageX
+      );
+      let dy = Math.abs(
+        e.nativeEvent.touches[0].pageY - e.nativeEvent.touches[1].pageY
+      );
+      let distant = Math.sqrt(dx * dx + dy * dy);
+      let scale = (distant / this.distant) * this.state.lastScale;
+      //check scale min to max hello
+      if (scale < this.props.maxScale && scale > this.props.minScale) {
+        this.setState({ scale, lastMovePinch: true });
+      }
+    }
+    // translate
+    else if (gestureState.numberActiveTouches === 1) {
+      if (this.state.lastMovePinch) {
+        gestureState.dx = 0;
+        gestureState.dy = 0;
+      }
+      let offsetX = this.state.lastX + gestureState.dx / this.state.scale;
+      let offsetY = this.state.lastY + gestureState.dy / this.state.scale;
+      // if ( offsetX < 0  || offsetY <  0 )
+      this.setState({ offsetX, offsetY, lastMovePinch: false });
+    }
+  };
+
+  render() {
+    return (
+      <View
+        {...this.gestureHandlers.panHandlers}
+        style={[
+          styles.container,
+          this.props.style,
+          {
+            transform: [
+              { scaleX: this.state.scale },
+              { scaleY: this.state.scale },
+              { translateX: this.state.offsetX },
+              { translateY: this.state.offsetY }
+            ]
+          }
+        ]}
+      >
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import { View, ImageBackground, Dimensions } from 'react-native';
 // import PlayArea from '../components/PlayArea';
-import PinchZoomView from 'react-native-pinch-zoom-view';
+// import PinchZoomView from 'react-native-pinch-zoom-view';
+import Test from '../components/Test';
 import MapGrid from '../graphics/temp/fullsize4x6grid15.png';
+import Detachment from '../components/Detachment/Detachment';
+
+const Window = Dimensions.get('window');
+const SCREEN_WIDTH = Window.width;
+const SCREEN_HEIGHT = SCREEN_WIDTH * 1.5;
+
 export default class TestScreen extends Component {
   render() {
     return (
-      <PinchZoomView>
-        <View>
-          <Image source={MapGrid} />
-        </View>
-      </PinchZoomView>
+
+      <Test>
+        <Detachment style={{zIndex: 99}} />
+        <ImageBackground source={MapGrid} style={{width: SCREEN_WIDTH, height: SCREEN_HEIGHT, zIndex: -5}} />
+      </Test>
+
     )
   }
 }
