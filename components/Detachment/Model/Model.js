d48fa6fb58a032a2d48c9d408f867902785515c5
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2019-02-15 04:24:27 -0800

    Fixed file name convention

--- components/Detachment/Model/Model.js
+++ components/Detachment/Model/Model.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import GhostModel from '../Model/ghostModel';
+import GhostModel from '../Model/GhostModel';
 import { 
     View,
     Text,
