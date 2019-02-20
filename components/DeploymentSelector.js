173c99af8876e38be8a69d96763d4aa3c951b5f1
Tristan Perera <tristan.e.perera@gmail.com>
2019-02-19 21:50:18 -0800

    set up deployment areas json

--- components/DeploymentSelector.js
+++ components/DeploymentSelector.js
@@ -4,6 +4,38 @@ import { Text, StyleSheet, View, ScrollView, Dimensions, Image, Button } from 'r
 import { AppConsumer } from '../storage/AppContext';
 import DeploymentAreas from '../utils/data/deploymentAreas.json';
 
+getDeploymentArea = (area) => {
+  switch (area) {
+    case "area1":
+    return require("../graphics/deployment/01-spearhead-assault.png")
+    break;
+
+    case "area2":
+    return require("../graphics/deployment/02-dawn-of-war.png")
+    break;
+
+    case "area3":
+    return require("../graphics/deployment/03-search-and-destroy.png")
+    break;
+
+    case "area4":
+    return require("../graphics/deployment/04-hammer-and-anvil.png")
+    break;
+
+    case "area5":
+    return require("../graphics/deployment/05-front-line-assault.png")
+    break;
+
+    case "area6":
+    return require("../graphics/deployment/06-vanguard-strike.png")
+    break;
+
+    default:
+      return console.log("no map")
+
+  } 
+}
+
 export default class DeploymentSelector extends Component {
   render() {
 
--- components/MapCardsSelector.js
+++ components/MapCardsSelector.js
@@ -10,76 +10,7 @@ import Maps from '../utils/data/maps.json';
 import { AppConsumer } from '../storage/AppContext';
 
 
-getImage = (image) => {
-  switch (image) {
-    case "map1":
-    return require("../graphics/maps/alienmarsh.png")
-    break;
-
-    case "map2":
-    return require("../graphics/maps/darklands.png")
-    break;
-
-    case "map3":
-    return require("../graphics/maps/deadlands.png")
-    break;
-
-    case "map4":
-    return require("../graphics/maps/drydesert.png")
-    break;
-
-    case "map5":
-    return require("../graphics/maps/DustPlains.png")
-    break;
-
-    case "map6":
-    return require("../graphics/maps/grassfield.png")
-    break;
-
-    case "map7":
-    return require("../graphics/maps/io.png")
-    break;
-
-    case "map8":
-    return require("../graphics/maps/mars.png")
-    break;
-
-    case "map9":
-    return require("../graphics/maps/muddyriver.png")
-    break;
-
-    case "map10":
-    return require("../graphics/maps/RockFormations.png")
-    break;
-
-    case "map11":
-    return require("../graphics/maps/SandDunes.png")
-    break;
-
-    case "map12":
-    return require("../graphics/maps/shallowsea.png")
-    break;
-
-    case "map13":
-    return require("../graphics/maps/venus.png")
-    break;
-
-    case "map14":
-    return require("../graphics/maps/wasteland.png")
-    break;
-
-    case "map15":
-    return require("../graphics/maps/wetlands.png")
-    break;
-
-    default:
-      return console.log("no map")
-
-  } 
-}
-
 const MapCardsSelector = () => {
-  
   
   return (
     <AppConsumer>
