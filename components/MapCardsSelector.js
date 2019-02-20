b317bdf9d6fee0b63f2c310d831c15f106590088
Tristan Perera <tristan.e.perera@gmail.com>
2019-02-19 21:25:01 -0800

    replaced images to map size

--- components/MapCardsSelector.js
+++ components/MapCardsSelector.js
@@ -13,28 +13,63 @@ import { AppConsumer } from '../storage/AppContext';
 getImage = (image) => {
   switch (image) {
     case "map1":
-    return require("../graphics/maps/City.jpg")
-    // return require("../graphics/maps/SandDunes.png")
+    return require("../graphics/maps/alienmarsh.png")
     break;
 
     case "map2":
-    return require("../graphics/maps/Forest.jpg")
+    return require("../graphics/maps/darklands.png")
     break;
 
     case "map3":
-    return require("../graphics/maps/Grassland.jpg")
+    return require("../graphics/maps/deadlands.png")
     break;
 
     case "map4":
-    return require("../graphics/maps/Snowfall.jpg")
+    return require("../graphics/maps/drydesert.png")
     break;
 
     case "map5":
-    return require("../graphics/maps/Swamp.jpg")
+    return require("../graphics/maps/DustPlains.png")
     break;
 
     case "map6":
-    return require("../graphics/maps/Valley.jpg")
+    return require("../graphics/maps/grassfield.png")
+    break;
+
+    case "map7":
+    return require("../graphics/maps/io.png")
+    break;
+
+    case "map8":
+    return require("../graphics/maps/mars.png")
+    break;
+
+    case "map9":
+    return require("../graphics/maps/muddyriver.png")
+    break;
+
+    case "map10":
+    return require("../graphics/maps/RockFormations.png")
+    break;
+
+    case "map11":
+    return require("../graphics/maps/SandDunes.png")
+    break;
+
+    case "map12":
+    return require("../graphics/maps/shallowsea.png")
+    break;
+
+    case "map13":
+    return require("../graphics/maps/venus.png")
+    break;
+
+    case "map14":
+    return require("../graphics/maps/wasteland.png")
+    break;
+
+    case "map15":
+    return require("../graphics/maps/wetlands.png")
     break;
 
     default:
