0b10ab8936816e9846a83c4a3526a9b96b888352
Tristan Perera <tristan.e.perera@gmail.com>
2019-03-08 00:08:42 -0800

    removed view detail

--- components/UnitCard.js
+++ components/UnitCard.js
@@ -1,74 +1,75 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, TouchableHighlight} from 'react-native'
-import { Container, Header, Content, Card, CardItem, Body, Button, Text } from 'native-base';
+import { StyleSheet, View, TouchableHighlight } from 'react-native'
+import { Card, CardItem, Body, Button, Text } from 'native-base';
 import { withNavigation } from 'react-navigation';
 import { AppConsumer } from '../storage/AppContext';
 
 class UnitCard extends Component {
-  render () {
+
+  render() {
     return (
       <AppConsumer>
         {(context) => (
           <TouchableHighlight onPress={this.props.cardPress}>
-          {context.state.playerOne.units.includes(this.props.modelName) ? (
-            <View>
-            <Card>
-              <CardItem>
-                <Body>
-                  <Text>Model Name: {this.props.modelName}</Text>
-                  <Text>BFRole: {this.props.bfRole}</Text>
-                  <Text>Points: {this.props.points}</Text>
-                  <Text>PL: {this.props.PL}</Text>
-                  {this.props.weapons.length >= 1 ? (
-                    this.props.weapons.length === 1 ? 
-                    (<Text>{this.props.weapons.length} Weapon Available</Text>) 
-                    : (<Text>{this.props.weapons.length} Weapons Available</Text>)
-                  ) : (
-                    console.log("")
-                  )}
-                    <Button 
-                        onPress={() => { this.props.navigation.navigate(this.props.path, {
-                        detail: this.props.detail
-                      })}}
-                    >
-                      <Text>View Detail</Text>
-                    </Button>
-                </Body>
-              </CardItem>
-            </Card>
-          </View>
-          ) : (
-            <View>
-              <Card>
-                <CardItem>
-                  <Body>
-                    <Text>Model Name: {this.props.modelName}</Text>
-                    <Text>BFRole: {this.props.bfRole}</Text>
-                    <Text>Points: {this.props.points}</Text>
-                    <Text>PL: {this.props.PL}</Text>
-                    {this.props.weapons.length >= 1 ? (
-                      this.props.weapons.length === 1 ? 
-                      (<Text>{this.props.weapons.length} Weapon Available</Text>) 
-                      : (<Text>{this.props.weapons.length} Weapons Available</Text>)
-                    ) : (
-                      console.log("")
-                    )}
-                      <Button 
-                          style={{marginTop: 5}}
+            {context.state.playerOne.units.includes(this.props.modelName) ? (
+              <View>
+                <Card>
+                  <CardItem>
+                    <Body>
+                      <Text>Model Name: {this.props.modelName}</Text>
+                      <Text>BFRole: {this.props.bfRole}</Text>
+                      <Text>Points: {this.props.points}</Text>
+                      <Text>PL: {this.props.PL}</Text>
+                      {this.props.weapons.length >= 1 ? (
+                        this.props.weapons.length === 1 ?
+                          (<Text>{this.props.weapons.length} Weapon Available</Text>)
+                          : (<Text>{this.props.weapons.length} Weapons Available</Text>)
+                      ) : (
+                          console.log("")
+                        )}
+                      {/* <Button
+                        onPress={() => {
+                          this.props.navigation.navigate(this.props.path, {
+                            detail: this.props.detail
+                          })
+                        }}
+                      >
+                        <Text>View Detail</Text>
+                      </Button> */}
+                    </Body>
+                  </CardItem>
+                </Card>
+              </View>
+            ) : (
+                <View>
+                  <Card>
+                    <CardItem>
+                      <Body>
+                        <Text>Model Name: {this.props.modelName}</Text>
+                        <Text>BFRole: {this.props.bfRole}</Text>
+                        <Text>Points: {this.props.points}</Text>
+                        <Text>PL: {this.props.PL}</Text>
+                        {this.props.weapons.length >= 1 ? (
+                          this.props.weapons.length === 1 ?
+                            (<Text>{this.props.weapons.length} Weapon Available</Text>)
+                            : (<Text>{this.props.weapons.length} Weapons Available</Text>)
+                        ) : (
+                            console.log("")
+                          )}
+                        {/* <Button
+                          style={{ marginTop: 5 }}
                           info
                           small
                           rounded
-                          onPress={() => { this.props.navigation.navigate(this.props.path, {
-                          detail: this.props.detail
-                        })}}
-                      >
-                        <Text>View Detail</Text>
-                      </Button>
-                  </Body>
-                </CardItem>
-              </Card>
-            </View>
-          )}
+                          onPress={this.props.press}
+                        >
+                          <Text>View Detail</Text>
+                        </Button> */}
+                      </Body>
+                    </CardItem>
+                  </Card>
+                </View>
+              )}
           </TouchableHighlight>
         )}
       </AppConsumer>
--- components/UnitSelector.js
+++ components/UnitSelector.js
@@ -6,111 +6,126 @@ import { AppProvider, AppConsumer } from '../storage/AppContext';
 import UnitCard from '../components/UnitCard';
 import Units from '../utils/data/units.json';
 import UnitsModal from './UnitsModal';
+import ModelDetailModal from './ModelDetailModal';
 
-const {width} = Dimensions.get("window")
+const { width } = Dimensions.get("window")
 const window = width
 
 export default class UnitSelector extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        isModalVisible: false
+      isUnitsModalVisible: false,
+      isDetailModalVisible: false,
+      modalData: {}
     }
   }
 
-  toggleModal = () => 
-    this.setState({ isModalVisible: !this.state.isModalVisible})
+  toggleUnitsModal = () =>
+    this.setState({ isUnitsModalVisible: !this.state.isUnitsModalVisible })
+
+  toggleDetailModal = () =>
+    this.setState({ isDetailModalVisible: !this.state.isDetailModalVisible })
 
   render() {
     return (
       <AppConsumer>
         {(context) => (
           // <Container>
-            <Grid>
-              <Row style={{display: "flex", justifyContent: "space-evenly"}} size={7.5}>
-                {/* <Col> */}
-                <Button
-                  style={styles.options}
-                  info
-                  
-                  rounded
-                  onPress={() => context.getFactionFromStorage(context.state.playerOne.faction)}
-                >
-                  <Text>Load Units</Text>
-                </Button>
-                {/* </Col> */}
-                {/* <Col> */}
-                <Button
-                  style={styles.options}
-                  info
-                 
-                  rounded
-                  onPress={this.toggleModal}
-                >
-                  <Text>View Units</Text>
-                </Button>
-                {/* </Col> */}
-              </Row>
+          <Grid>
+            <Row style={{ display: "flex", justifyContent: "space-evenly" }} size={7.5}>
+              {/* <Col> */}
+              <Button
+                style={styles.options}
+                info
 
-              <UnitsModal 
-                isModalVisible={this.state.isModalVisible}
-                press={this.toggleModal}
-              />
+                rounded
+                onPress={() => context.getFactionFromStorage(context.state.playerOne.faction)}
+              >
+                <Text>Load Units</Text>
+              </Button>
+              {/* </Col> */}
+              {/* <Col> */}
+              <Button
+                style={styles.options}
+                info
 
-              <Row size={94}>
-                <Content>
-                  {context.state.BSData.data.map(unit => (
+                rounded
+                onPress={this.toggleUnitsModal}
+              >
+                <Text>View Units</Text>
+              </Button>
+              {/* </Col> */}
+            </Row>
 
-                    (unit.type === "model") ? (
+            <UnitsModal
+              isModalVisible={this.state.isUnitsModalVisible}
+              press={this.toggleUnitsModal}
+            />
 
-                      <UnitCard
-                        key={unit["id"]}
-                        detail={unit.abilities}
-                        path="FactionDetail"
-                        modelName={unit.name}
-                        bfRole={unit.bf_role}
-                        points={unit.pts}
-                        PL={unit.PL}
-                        weapons={unit.weapons}
-                        cardPress={() => {
-                          context.setUnit({ 
-                            id: unit.id, 
-                            style: "modelKiwi",
-                            text: "",
-                            modelName: unit.name,
-                            player: 1,
-                            x: 0,
-                            y: 0,
-                            a: unit.profile.A,
-                            bs: unit.profile.BS,
-                            ld: unit.profile.Ld,
-                            m: unit.profile.M,
-                            s: unit.profile.S,
-                            sv: unit.profile.Sv,
-                            t: unit.profile.T,
-                            ws: unit.profile.WS,
-                            wound: unit.profile.W,
-                            ability: unit.abilities,
-                            inRange: false,
-                            weapons: unit.weapons
-                          })
-                        }}
-                      >{}</UnitCard>
+            <ModelDetailModal
+              modalData={this.state.modalData}
+              isModalVisible={this.state.isDetailModalVisible}
+              press={this.toggleDetailModal}
+            />
 
-                    ) : (console.log(""))
-                  ))}
-                </Content>
+            <Row size={94}>
+              <Content>
+                {context.state.BSData.data.map(unit => (
 
-              </Row>
+                  (unit.type === "model") ? (
 
-              {/* <Row size={20}>
+                    <UnitCard
+                      key={unit["id"]}
+                      detail={unit.abilities}
+                      path="FactionDetail"
+                      modelName={unit.name}
+                      bfRole={unit.bf_role}
+                      points={unit.pts}
+                      PL={unit.PL}
+                      weapons={unit.weapons}
+                      cardPress={() => {
+                        context.setUnit({
+                          id: unit.id,
+                          style: "modelKiwi",
+                          text: "",
+                          modelName: unit.name,
+                          player: 1,
+                          x: 0,
+                          y: 0,
+                          a: unit.profile.A,
+                          bs: unit.profile.BS,
+                          ld: unit.profile.Ld,
+                          m: unit.profile.M,
+                          s: unit.profile.S,
+                          sv: unit.profile.Sv,
+                          t: unit.profile.T,
+                          ws: unit.profile.WS,
+                          wound: unit.profile.W,
+                          ability: unit.abilities,
+                          inRange: false,
+                          weapons: unit.weapons
+                        })
+                      }}
+                      onPress={() => this.setState({ modalData: unit})}
+                      press={this.toggleDetailModal}
+                      
+                    >{}</UnitCard>
+
+                  ) : (console.log(""))
+                ))}
+              </Content>
+
+            </Row>
+
+            {/* <Row size={20}>
 
                 {context.state.playerOne.units.map(model => {
                   <Text>{model.name}</Text>
                 })}
               </Row> */}
 
-            </Grid>
+          </Grid>
           // </Container>
         )}
       </AppConsumer>
@@ -121,7 +136,7 @@ export default class UnitSelector extends Component {
 const styles = StyleSheet.create({
   options: {
     width: window / 2.2,
-    display: "flex", 
+    display: "flex",
     justifyContent: "space-evenly"
   }
 });
